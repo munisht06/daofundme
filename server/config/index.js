bc3f22be4710aa2b2a26654a954b4bb827f3bbb5
munisht06 <munishtanwar77@gmail.com>
2022-09-10 02:11:24 -0400

    auth0 config file fixed

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,8 @@
 const db = require('./db');
+const auth0 = require('./auth0');
 
 // Exports all configuration functions
 module.exports = {
 	dbClient: db,
+	auth0: auth0,
 };
--- server/configs/index.js
+++
@@ -1,5 +0,0 @@
-const auth0 = require("./auth0");
-
-module.exports = {
-  auth0: auth0,
-};
