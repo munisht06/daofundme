d61f854c68d2dea9e81632dcf5935d4e05fcac4d
Reynaldo <salcedoreynaldo5@gmail.com>
2022-09-10 00:50:20 -0400

    Added mongodb and tested connection

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {});
