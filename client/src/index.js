83888b8a3305bfd5c79c300176b5adcd9db4621d
Reynaldo <salcedoreynaldo5@gmail.com>
2022-09-10 23:23:30 -0400

    Fixed auth0

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+	<BrowserRouter>
+		<Auth0Provider
+			domain="daofundme-auth.us.auth0.com"
+			clientId="L1udU93ILcfPvYdq47txvYLmdLI2WnxM"
+			redirectUri={window.location.origin}
+		>
+			<App />
+		</Auth0Provider>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
